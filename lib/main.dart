d057d44ae27067bc7a31138893ea81ce0b4f777a
pkgit-tech <80440153+pkgit-tech@users.noreply.github.com>
2021-05-31 00:47:28 +0530

    Update main.dart
    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../Screen/bottomtabbar.dart';
+import 'Screen/bottomtabbar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
